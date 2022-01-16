035590dcabc640887f94e2f6b8c2aee412a84699
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-01-15 23:05:03 -0800

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+var firstElement = languages;
+console.log('the first element in the array is ' + firstElement[0]);
+
+var thirdElement = languages[2];
+console.log('the third element in the array is ' + thirdElement);
+
+var length = languages.length;
+console.log('the length of the array is ' + length);
+
+var lastElement = languages[languages.length - 1];
+console.log('the last element in the array is ' + lastElement);
